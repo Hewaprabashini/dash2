af6082c7d7d3359a883d83ab5e61bd3acca7d866
Hewaprabashini <160015389+Hewaprabashini@users.noreply.github.com>
2024-05-08 18:36:03 +0530

    Update stree.py

--- stree.py
+++ stree.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import pandas as pd
 
 st.set_page_config(page_title="Superstore", page_icon=":bar_chart:", layout="wide")
-st.title("Dashboard of global super store")
+st.title("Dashboard of global superstore")
 
 f1 = st.file_uploader(':file_folder: Upload a file', type=(["csv", "xlsx"]))
 if f1 is not None:
