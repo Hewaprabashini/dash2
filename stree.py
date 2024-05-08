8a5d32a1656e1998ef8f3b21d09af52428092f70
Hewaprabashini <160015389+Hewaprabashini@users.noreply.github.com>
2024-05-08 23:43:41 +0530

    Update stree.py

--- stree.py
+++ stree.py
@@ -25,7 +25,7 @@ with col1 :
 with col2 :
     date2 = pd.to_datetime(st.date_input("End Date", endDate))
 
-df = df[(df[" Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
 
 st. sidebar.header("Choose your filter:")
 
